fb9c34fb38f8392af96c61ca8a68b1b1f5766056
Matt Simpson <simpson@queensu.ca>
2017-06-15 09:58:30 -0700

    Updated Docker configuration.

--- resources/docker/entrada/mariadb/entrada.sql
+++ resources/docker/entrada/mariadb/entrada.sql
@@ -1,14 +1,19 @@
 UPDATE `mysql`.`user` SET `Password` = PASSWORD('password') WHERE `User` = "root";
-CREATE USER 'root'@'gateway' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'gateway' WITH GRANT OPTION;
+CREATE USER 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 CREATE DATABASE `entrada` CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `entrada_auth` CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE DATABASE `entrada_clerkship` CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE USER 'entrada'@'gateway' IDENTIFIED BY 'password';
-GRANT ALL ON `entrada`.* TO 'entrada'@'gateway';
-GRANT ALL ON `entrada_auth`.* TO 'entrada'@'gateway';
-GRANT ALL ON `entrada_clerkship`.* TO 'entrada'@'gateway';
+CREATE USER 'entrada'@'%' IDENTIFIED BY 'password';
+CREATE USER 'entrada'@'localhost' IDENTIFIED BY 'password';
+UPDATE `mysql`.`user` SET `max_questions` = 0, `max_updates` = 0, `max_connections` = 0 WHERE `User` = 'entrada' AND `Host` IN ('%', 'localhost');
+GRANT ALL ON `entrada`.* TO 'entrada'@'%';
+GRANT ALL ON `entrada_auth`.* TO 'entrada'@'%';
+GRANT ALL ON `entrada_clerkship`.* TO 'entrada'@'%';
+GRANT ALL ON `entrada`.* TO 'entrada'@'localhost';
+GRANT ALL ON `entrada_auth`.* TO 'entrada'@'localhost';
+GRANT ALL ON `entrada_clerkship`.* TO 'entrada'@'localhost';
 
 FLUSH PRIVILEGES;
